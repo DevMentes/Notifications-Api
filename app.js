3f1794295232e70136b725f15d445e7f1d5c3dbb
Camilo Alaniz <kmilo93s@gmail.com>
2019-01-05 19:35:57 -0300

    added slack send message integration

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const axios = require('axios');
 
 app.get('/', (req, res) => res.send('Hello world'));
 
+app.post('/message', async(req, res) => {
+
+    try{
+        let response = await axios.post(
+            'https://hooks.slack.com/services/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',
+            {
+                text: req.body.message
+            }
+        );
+        res.json(
+            {
+                'message':response
+            }
+            );
+    }catch (error) {
+        res.json(
+            {
+                'error':error.message
+            }
+        );
+    }
+});
 app.listen(port, () => console.log(`App listening from port ${port}`));
